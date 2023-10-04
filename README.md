f1a4792a6a26340c23a5599133f081d7df656f08
Maxime <maxime.britto@edu.devinci.fr>
2023-10-04 16:56:59 +0200

    AddGridTableWithData

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
-# React + Vite
+#Aggrid boilerplate
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This project present some diffrents features qith vite.js and with this project you have the base to create a aggrid project 
 
-Currently, two official plugins are available:
+## Features
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Vite: Fast, optimized, and next-generation front-end tooling.
+- ESLint: Ensure your code follows best practices and catches common errors.
+- Jest: Delightful JavaScript testing for React applications.
+- Husky: Ensure code quality by running ESLint and tests before committing.
+- Prettier: Code formatter to maintain a consistent code style.
+- React: A declarative, efficient, and flexible JavaScript library for building user interfaces.
+- Material-UI (MUI): React components that implement Google's Material Design.
+- Emotion: A popular library for writing styles with JavaScript.
+- Sass: An extension of CSS that adds power and elegance to the basic language.
+- React Query: Performant and powerful data fetching library for React.
+- Axios: Promise-based HTTP client for the browser and Node.js.
+- Iconify: Easily include icons in your project from multiple icon sets.
+- date-fns: Modern JavaScript date utility library.
+- aggrid: powerful tools to create some tab and easy filter
+-chartjs: good tools to create graph
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,73 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { AgGridReact } from 'ag-grid-react'
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { Icon } from '@iconify/react';
+import { Bar } from 'react-chartjs-2';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
+const data = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: 'Couleur preferé des francais',
+      data: ["12", "19", "3"," 5"," 2", "3"],
+    },
+  ]
+}
+
+const Car = [
+  { label: 'Porsche', year: 1994 },
+  { label: 'Ford', year: 1972 },
+  { label: 'Toyota', year: 1974 },
+]
 function App() {
   
-const rowData = [
-  {make:'Toyota', model:'Celica', price:35000},
-  {make:'Ford', model:'focus', price:45000},
-  {make:'BMW', model:'4 Series', price:60000}
-];
-const columnDefs = [
-    { field: 'make' },
+  const[rowData,setRowData] = useState([
+]);
+
+const [columnDefs, setColumnDefs] = useState([
+    { field: 'make', columnGroupShow: 'open' },
     { field: 'model' },
     { field: 'price' }
-];
+]);
+
+useEffect(() => {
+fetch('https://www.ag-grid.com/example-assets/row-data.json')
+.then(result => result.json())
+.then(rowData => setRowData(rowData))
+console.log(rowData)
+},[])
   return (
-    <div className='ag-theme-alpine'style={{height: 500}}>
+    <div className='ag-theme-alpine'style={{
+      display: 'flex',
+      flexDirection: 'column',
+      
+      height: 500, width: 625}}>
+  
+      <h1 style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'}}> <Icon width="50px" icon="meteocons:barometer" />CarTab <Icon width="50px" icon="meteocons:barometer" /></h1>
+              <Autocomplete
+      disablePortal
+      id="combo-box-demo"
+      options={Car}
+      sx={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label="Car" />}
+    />
 <AgGridReact
+
 rowData={rowData}
 columnDefs={columnDefs}
+
 />
+
+
     </div>
       
   )
